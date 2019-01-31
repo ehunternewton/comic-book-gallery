b66a3b49f1cd57d31f6d24c6ea833e06bc82b1eb
ehunternewton@gmail.com <ehunternewton@gmail.com>
2019-01-31 16:45:11 -0600

    Added ComicBook Detail view

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,14 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult detail()
+        public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("hello from comic book controller");
-            
+            return View();
         }
     }
 }
